1fcf1ff3728197d1f8643692e3626991b1973404
Viktor Shilay <viktor.shilay@gmail.com>
2022-09-09 22:56:52 +0300

    init consumer module

---
+++ consumer/src/test/java/by/shilay/app/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shilay.app.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
