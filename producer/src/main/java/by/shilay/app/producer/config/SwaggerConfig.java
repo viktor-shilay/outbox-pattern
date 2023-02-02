d741b4418a089d12ae212173e86cf2fe412eb72d
Viktor Shilay <viktor.shilay@gmail.com>
2023-02-02 14:03:57 +0300

    added swagger

---
+++ producer/src/main/java/by/shilay/app/producer/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package by.shilay.app.producer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("by.shilay.app.producer"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
